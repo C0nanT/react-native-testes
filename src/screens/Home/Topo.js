578360c4373dc129d33e3c0fa153a324177a282d
Conan Torres <conantorreswf@gmail.com>
2024-04-07 18:33:10 -0300

    commit inicial

---
+++ src/screens/Home/Topo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+export default function Topo() {
+  return (
+    <View>
+      <Text>Topo</Text>
+      <Text>Topo</Text>
+      <Text>Topo</Text>
+      <Text>Topo</Text>
+      <Text>Topo</Text>
+    </View>
+  );
+}
---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Topo from "./Topo.js";
+export default function Home() {
+  return (
+    <View>
+      <Topo />
+    </View>
+  );
+}
